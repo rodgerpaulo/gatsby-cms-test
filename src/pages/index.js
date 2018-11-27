d7b702dc83bcc21cda44fdc213d8fa5f112a87a3
Roger Ramos <rodgerpaulo@hotmail.com>
2018-11-27 15:11:50 -0200

    Adds example rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default ({ data }) =>  {
+    const { edges } = data.allMarkdownRemark
+
+    return (
+        edges.map(({node}) => (
+            <ul>
+                <li>id: {node.id}</li>
+                <li>path: <a href={node.frontmatter.path}>{node.frontmatter.path}</a></li>
+                <li>title: {node.frontmatter.title}</li>
+                <li>date: {node.frontmatter.date}</li>
+            </ul>
+        ))
+    )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      limit: 6
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 75)
+          frontmatter {
+            title
+            path
+            date(formatString: "MM.DD.YYYY")
+          }
+        }
+      }
+    }
+  }
+`;
